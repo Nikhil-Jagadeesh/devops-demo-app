96ffb80b17ec0b62a85d7525aba8980588b591de
Nikhil-Jagadeesh <70753271+Nikhil-Jagadeesh@users.noreply.github.com>
2025-09-22 12:28:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.get("/")
 def hello():
-    return "Hello from Jenkins + SonarQube + Minikube!"
+    return "Hello from Jenkins + SonarQube + Minikube!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081)
